71230717e2a1dd34a2c19a4664b34b2a21acdad6
Shrajan <jainshrajan482@gmail.com>
2024-08-05 12:01:04 +0530

    deployment configurations

--- Frontend-Snippets-Hub/vite.config.js
+++ Frontend-Snippets-Hub/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://shrajan9696.github.io/React_Code_Snippets"
+  base: "https://shrajan9696.github.io/React_Code_Snippets/Frontend-Snippets-Hub"
 })
